48b110cf40f3a367395e378c434bbe3ed6a26023
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2025-11-28 07:28:57 -0300

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,23 @@
 // src/app/layout.js
-
-import { useEffect } from 'react';
 import "./globals.css";
+import ServiceWorkerRegister from "@/components/ServiceWorkerRegister";
+
+export const metadata = {
+  title: "Ordengo Garçom",
+  description: "Sistema do garçom",
+};
 
 export default function RootLayout({ children }) {
-  
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('/service-worker.js')
-        .then((registration) => console.log('Scope: ', registration.scope))
-        .catch((err) => console.log('Service Worker Registration Failed: ', err));
-    }
-  }, []);
-
   return (
     <html lang="pt-BR">
       <head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#df0024" />
       </head>
-      <body>{children}</body>
+      <body>
+        <ServiceWorkerRegister />
+        {children}
+      </body>
     </html>
   );
 }
